7600258c0d540623b9e8c63a4053d3913dd4d1b2
Sagar585 <sagar.nb@somaiya.edu>
2023-09-29 18:23:39 +0530

    hehe

--- frontend/src/redux/cart/cart.reducers.js
+++ frontend/src/redux/cart/cart.reducers.js
@@ -32,6 +32,7 @@ export default function cartreducer(state = initialState, action) {
       };
     }
     case CLEAR_ITEM_FROM_CART: {
+      // pajdoae
       localStorage.setItem("cartItems", JSON.stringify([]));
       return {
         ...state,
