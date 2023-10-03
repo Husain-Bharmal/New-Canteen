4a6dd45cbf712d1678838af382742f1c013875e7
Sagar585 <sagar.nb@somaiya.edu>
2023-10-03 21:06:39 +0530

    dashboard is working nicely and user name gets displaced in wellcome message and also admin and user see different functionalities

--- frontend/src/components/features_btn/welcome/Welcome.jsx
+++ frontend/src/components/features_btn/welcome/Welcome.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './Welcome.css'; // Import your CSS file for styling
 
-const Welcome = () => {
+const Welcome = ({ userName }) => {
   return (
     <div className="welcome-page">
       <div className="welcome-message">
-        <div className="admin-name">Welcome, Admin!</div>
+        <div className="admin-name">Welcome, {userName}!</div>
         <p>Thank you for using the Canteen Management System.</p>
         {/* Add any additional content or buttons here */}
       </div>
--- frontend/src/pages/dashboard/Dashboard.jsx
+++ frontend/src/pages/dashboard/Dashboard.jsx
@@ -1,72 +1,109 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import Loader from "../../components/loader/Loader";
-import { connect } from "react-redux";
-import { getAdminORders } from "../../redux/order/order.actions";
-import AdminDashboard from "../../components/admin-dashboard/AdminDashboard";
-// import UserDashboard from "../../components/user-dashboard/UserDashboard";
-import GetUsersBtn from "../../components/features_btn/users/Users"
-import Welcome from "../../components/features_btn/welcome/Welcome";
-import Sales from "../../components/features_btn/sales_report/sales"
+  import React, { useEffect } from "react";
+  import { useState } from "react";
+  import Loader from "../../components/loader/Loader";
+  import { connect } from "react-redux";
+  import { getAdminORders } from "../../redux/order/order.actions";
+  import AdminDashboard from "../../components/admin-dashboard/AdminDashboard";
+  // import UserDashboard from "../../components/user-dashboard/UserDashboard";
+  import GetUsersBtn from "../../components/features_btn/users/Users";
+  import Welcome from "../../components/features_btn/welcome/Welcome";
+  import Sales from "../../components/features_btn/sales_report/sales";
 
-import "./Dashboard.css";
+  import "./Dashboard.css";
 
-const Dashboard = ({ getAdminORders, user, loading }) => {
-  const [condition, setCondition] = useState("component0");
+  const Dashboard = ({ getAdminORders, user, loading }) => {
+    const [condition, setCondition] = useState("component0");
 
-  const componentMap = {
-    component0:<Welcome/>,
-    component1: < GetUsersBtn/>,
-    component2: <AdminDashboard />,
-    component3:<Sales/>,
-    // Add more components and conditions here
-  };
+    const componentMap = {
+      component1: <GetUsersBtn />,
+      component2: <AdminDashboard />,
+      component3: <Sales />,
+      // Add more components and conditions here
+    };
 
-  const renderComponent = () => {
-    return componentMap[condition] || null; // Return null for unknown conditions
-  };
+    const renderComponent = () => {
+      if (user && user.role === "admin") {
+        return <AdminDashboard />;
+      } else {
+        if (["component1", "component2", "component3"].includes(condition)) {
+          // If the condition is one of the buttons that should hide the Welcome component
+          return componentMap[condition] || null;
+        } else {
+          return <Welcome userName={user?.name} />;
+        }
+      }
+    };
+    
+        
+    
+    const handleButtonClick = (newCondition) => {
+      setCondition(newCondition);
+      console.log("New condition:", newCondition);
+    };
+
+    useEffect(() => {
+      getAdminORders();
+    }, [getAdminORders]);
 
 
-  useEffect(() => {
-    getAdminORders();
-  }, [getAdminORders]);
 
-  return (
-    <div>
-      {loading && <Loader />}
-      <div className="dashboard-root">
-        <div className="profile-details">
-          <div className="profile">
-            
-            <p>
-              <b>User: </b> {user?.name}
-            </p>
-            <p>
-              <b>Branch: </b> {user?.branch}
-            </p>
-            <p>
-              <b>Role: </b>
-              {user?.role}
-            </p>
+    return (
+      <div>
+        {loading && <Loader />}
+        <div className="dashboard-root">
+          <div className="profile-details">
+            <div className="profile">
+              <p>
+                <b>User: </b> {user?.name}
+              </p>
+              <p>
+                <b>Branch: </b> {user?.branch}
+              </p>
+              <p>
+                <b>Role: </b>
+                {user?.role}
+              </p>
+            </div>
+            <div className="features_btn_div">
+    {user && user.role === "teacher" && (
+      <>
+        <button
+          className="features_btn"
+          onClick={() => handleButtonClick("component1")}
+        >
+          Users
+        </button>
+        <button
+          className="features_btn"
+          onClick={() => handleButtonClick("component3")}
+        >
+          Sales
+        </button>
+      </>
+    )}
+    <button
+      className="features_btn"
+      onClick={() => handleButtonClick("component2")}
+    >
+      Orders
+    </button>
+  </div>
+
           </div>
-          <div className="features_btn_div">
-          <button className="features_btn" onClick={() => setCondition("component1")}>Users</button>
-          <button className="features_btn" onClick={() => setCondition("component2")}>Orders</button>
-          <button className="features_btn" onClick={() => setCondition("component3")}>Sales</button>
+          <div>
+            <h1 className="dashboard-text">Dashboard</h1>
+            {/* <Welcome userName={user?.name} /> */}
+            {renderComponent()}
           </div>
-        </div>
-        <div>
-          <h1 className="dashboard-text">Dashboard</h1>
-          {renderComponent()}
         </div>
       </div>
-    </div>
-  );
-};
-const mapStateToProps = (state) => ({
-  user: state.auth.user,
-  orders: state.order.orders,
-  loading: state.order.loading,
-});
+    );
+  };
 
-export default connect(mapStateToProps, { getAdminORders })(Dashboard);
+  const mapStateToProps = (state) => ({
+    user: state.auth.user,
+    orders: state.order.orders,
+    loading: state.order.loading,
+  });
+
+  export default connect(mapStateToProps, { getAdminORders })(Dashboard);
