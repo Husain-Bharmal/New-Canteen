7a7c5099e0de1be84ff929f3fe0fa2e1f5a187b5
Sagar585 <sagar.nb@somaiya.edu>
2023-10-06 17:23:06 +0530

    dashboard looks good added chart added most selling item

--- frontend/src/components/features_btn/sales_report/sales.jsx
+++ frontend/src/components/features_btn/sales_report/sales.jsx
@@ -1,41 +1,49 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { getAllFoodItems } from "../../../redux/food/food.actions";
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from "recharts";
 import "./sales.css";
-import {
-  BarChart,
-  Bar,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-  ResponsiveContainer,
-} from "recharts";
-
 const FoodItemTable = ({ foods, match, getAllFoodItems }) => {
-  // Log the current state of food items
-  console.log("food items are:", foods);
-
-  // Use useEffect to fetch all food items when the component mounts or when 'match' changes
   useEffect(() => {
     getAllFoodItems("all");
   }, [getAllFoodItems, match]);
 
-  // Data for the bar chart (you can customize this based on your data structure)
-  const data = foods.map((item) => ({
-    name: item.name,
-    quantitySold: item.totalItemSold,
-    totalEarnings: item.totalEarnings,
-  }));
+  // Data for the bar chart
+  const [barChartData, setBarChartData] = useState([]);
+
+  useEffect(() => {
+    // Transform foods data for the bar chart
+    const transformedData = foods.map((item) => ({
+      name: item.name,
+      quantitySold: item.totalItemSold,
+      totalEarnings: item.totalEarnings,
+    }));
+    setBarChartData(transformedData);
+  }, [foods]);
+
+  // Find the most selling product
+  const mostSellingProduct = foods.reduce((maxProduct, currentProduct) => {
+    return currentProduct.totalItemSold > maxProduct.totalItemSold ? currentProduct : maxProduct;
+  }, foods[0]);
 
   return (
     <div className="food-item-table">
       <h2>Food Item Sales Report</h2>
-      
-      {/* Table to display food item sales information */}
+
+      {/* Display most selling product if it exists */}
+      {mostSellingProduct && (
+        <div className="most-selling-product">
+          <h3>Most Selling Product:</h3>
+          <div className="most-selling-product-details">
+          <p>{mostSellingProduct.name}</p>
+          <p>Quantity Sold: {mostSellingProduct.totalItemSold}</p>
+          <p>Total Earnings: {mostSellingProduct.totalEarnings} Rs</p>
+          </div>
+        </div>
+      )}
+
+      {/* Table */}
       <table>
-        {/* Table header */}
         <thead>
           <tr>
             <th>Food Item</th>
@@ -43,13 +51,11 @@ const FoodItemTable = ({ foods, match, getAllFoodItems }) => {
             <th>Total Amount Earned</th>
           </tr>
         </thead>
-        {/* Table body with data from 'foods' array */}
         <tbody>
           {foods.map((item, index) => (
             <tr key={index}>
               <td>{item.name}</td>
               <td>{item.totalItemSold}</td>
-              {/* Display total earnings or '0 Rs' if it's zero */}
               <td>{item.totalEarnings !== 0 ? `${item.totalEarnings} Rs` : '0 Rs'}</td>
             </tr>
           ))}
@@ -57,17 +63,31 @@ const FoodItemTable = ({ foods, match, getAllFoodItems }) => {
       </table>
 
       {/* Bar Chart */}
-      <ResponsiveContainer width="100%" height={300}>
-        <BarChart data={data}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Bar dataKey="quantitySold" fill="#8884d8" />
-          <Bar dataKey="totalEarnings" fill="#82ca9d" />
-        </BarChart>
-      </ResponsiveContainer>
+      <div className="bar-chart">
+        {/* Bar Chart for Quantity Sold */}
+        <div className="bar-chart-container bar-chart-container-quantity">
+          <BarChart width={500} height={300} data={barChartData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="quantitySold" fill="#8884d8" />
+          </BarChart>
+        </div>
+
+        {/* Bar Chart for Total Earnings */}
+        <div className="bar-chart-container bar-chart-container-earnings">
+          <BarChart width={500} height={300} data={barChartData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="name" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="totalEarnings" fill="#82ca9d" />
+          </BarChart>
+        </div>
+      </div>
     </div>
   );
 };
